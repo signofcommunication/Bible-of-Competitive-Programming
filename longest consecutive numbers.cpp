47fa7dcc691c65e7c4df0d46a27af8bf62aca265
Jhvonsky <70561984+signofcommunication@users.noreply.github.com>
2022-04-19 19:27:12 +0700

    Create longest consecutive numbers.cpp

---
+++ longest consecutive numbers.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+const int Nmax = 100001;
+int a[Nmax],n;
+bool fr[Nmax];
+
+int longestConsecNums() {
+    int ans = 0;
+
+    for(int Left = 1; Left <= n; Left++) {
+        for(int i = 1; i <= n; i++) {
+            fr[i] = false;
+        }
+
+        int Min = a[Left];
+        int Max = a[Left];
+
+        for(int Right = Left; Right <= n; Right++) {
+            if(fr[a[Right]] == true) {
+                break;
+            }
+
+            fr[a[Right]] = true;;
+            Min = min(Min,a[Right]);
+            Max = max(Max,a[Right]);
+
+            if((Right - Left) == (Max - Min)) {
+                ans = max(ans, Right - Left + 1);
+            }
+        }
+    }
+
+    return ans;
+}
+
+int main() {
+    cin >> n;
+    for(int i = 1; i <= n; i++) {
+        cin >> a[i];
+    }
+    cout << longestConsecNums();
+
+    return 0;
+}
